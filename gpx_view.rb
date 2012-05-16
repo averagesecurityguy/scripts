8a3e76378632739b58d2fbfe228a05169617541a
Stephen Haywood <stephen@averagesecurityguy.info>
2012-05-16 15:48:07 -0400

    Updated gpx_view.rb to be able to sort the waps by mac, ssid, channel, or encryption.

--- gpx_view.rb
+++ gpx_view.rb
@@ -18,40 +18,135 @@
 require 'rubygems'
 require 'libxml'
 require 'rextable'
+require 'optparse'
 
-Wap = Struct.new(:ssid, :power, :channel, :encryption, :quality)
+Wap = Struct.new(:mac, :ssid, :power, :channel, :encryption, :quality)
 
-def usage
-    puts "gpx_view reads the gpxfile and outputs the wireless access points "
-    puts "identified in the file. The output format can be short or long. "
-    puts "The short format prints each wireless access point once while the "
-    puts "long format prints each instance of the wireless access point. The "
-    puts "default format is short. "
-    puts
-    puts "Usage: gpx_view.rb gpxfile [short|long]"
+def sort_wap_list(wap_list, sort)
+	case sort.downcase
+		when 'ssid'
+			return wap_list.sort_by {|w| [w.ssid, w.mac]}
+		when 'mac'
+			return wap_list.sort_by {|w| w.mac}
+		when 'channel'
+			return wap_list.sort_by {|w| [w.channel.to_i, w.mac]}
+		when 'encryption'
+			return wap_list.sort_by {|w| [w.encryption, w.mac]}
+	end
 end
 
-if ARGV.length < 1 then
-    usage
-    exit(1)
+def split_wap_list(wap_list, split)
+	hash = Hash.new
+	wap_list.each do |entry|
+		case split.downcase
+			when 'ssid'
+				key = entry.ssid
+			when 'mac'
+				key = entry.mac
+			when 'channel'
+				key = entry.channel
+			when 'encryption'
+				key = entry.encryption
+		end
+	
+		unless hash[key]
+			hash[key] = Array.new
+			hash[key] << entry
+		else
+			hash[key] << entry
+		end
+		
+	end
+
+	return hash
 end
 
-case ARGV[1]
-when nil
-    format = "short"
-when "short"
-    format = "short"
-when "long"
-    format = "long"
-else
-    usage
-    exit(1)
+#-----------------------------------------------------------------------------
+# Process command line arguments
+#-----------------------------------------------------------------------------
+options = {}
+
+optparse = OptionParser.new do|opts|
+	# Usage banner
+	opts.banner =  "Gpx_view is a ruby script that reads a GPX file and outputs "
+	opts.banner += "the wireless access points identified in the file. The "
+	opts.banner += "output format can be short or long. The short format prints "
+	opts.banner += "each wireless access point once while the long format "
+	opts.banner += "prints each instance of the wireless access point. The " 
+	opts.banner += "default format is short. Wireless access points can be "
+	opts.banner += "sorted by MAC address, SSID, channel or encryption type.\n\n"
+
+	# GPX file to read
+	options[:gpx] = ""
+	opts.on('-g', '--gpx FILE', "GPX file to parse.") do |g|
+		options[:gpx] = g
+	end
+	
+	# Report format to use
+	options[:format] = "short"
+	opts.on( '-f', '--format FORMAT', "Report format to use. Must be 'short' or 'long'" ) do |f|
+		options[:format] = f
+	end
+
+	# Sort method
+	options[:sort] = "mac"
+	opts.on( '-s', '--sort METHOD', "Sort method for access points. Must be 'mac', 'ssid', 'channel', or 'encryption'" ) do |s|
+		options[:sort] = s
+	end
+
+	# This displays the help screen.
+	opts.on( '-h', '--help', 'Display this screen' ) do
+		puts opts
+		exit
+	end
 end
 
-doc = LibXML::XML::Document.file(ARGV[0])
-waps = Hash.new
+optparse.parse!
+
+
+#-----------------------------------------------------------------------------
+# Validate command line arguments
+#-----------------------------------------------------------------------------
+
+# Ensure GPX file exists
+unless File.exists?(options[:gpx])
+	puts "[ERROR] GPX file does not exist."
+	exit
+end
+
+# Ensure GPX file is a file
+unless File.file?(options[:gpx])
+	puts "[ERROR] GPX file is not a file."
+	exit
+end
+
+# Ensure GPX file is not empty
+if File.zero?(options[:gpx])
+	puts "[ERROR] GPX file is empty."
+	exit
+end
+
+# Ensure a valid sort option was given
+unless ['ssid', 'mac', 'channel', 'encryption'].include?(options[:sort].downcase)
+	raise OptionParser::InvalidArgument("The sort value must be 'ssid', 'mac', 'channel', or 'encryption'.")
+end
+
+# Ensure a valid format option was given
+unless ['short', 'long'].include?(options[:format].downcase)
+	raise OptionParser::InvalidArgument("The format value must be either 'short' or 'long'.")
+end
+
+
+#-----------------------------------------------------------------------------
+# Begin Main Program
+#-----------------------------------------------------------------------------
+
+# Open the GPX file with libxml
+doc = LibXML::XML::Document.file(options[:gpx])
+wap_list = Array.new
 entries = doc.root.find('wpt')
 
+# Process WAP entries
 entries.each do |wpt|
     mac = wpt.find_first('extensions/MAC').content
     ssid = wpt.find_first('extensions/SSID').content
@@ -62,29 +157,43 @@ entries.each do |wpt|
     encryption = wpt.find_first('extensions/privacy').content
     quality = wpt.find_first('extensions/signalQuality').content
     
-    wap = Wap.new(ssid, power, channel, encryption, quality) 
-    
-    if not waps[mac] then
-        waps[mac] = Array.new
-        waps[mac] << wap
-    else
-        waps[mac] << wap
-    end
+    wap = Wap.new(mac, ssid, power, channel, encryption, quality) 
+	
+	wap_list << wap
+	
 end
 
-if format == "short"
-    tbl = RexTable::Table.new('Columns' => ["MAC Address", "SSID", "RSSI", "Channel", "Encryption", "Quality"])
-    waps.each do |k, v|
-        tbl << [k, v[0].ssid, v[0].power, v[0].channel, v[0].encryption, v[0].quality]
+# Sort the wap_list based on the sort value
+wap_list = sort_wap_list(wap_list, options[:sort])
+
+if options[:format].downcase == 'short'
+    tbl = RexTable::Table.new('Columns' => ["MAC", "SSID", "Power", "Channel", "Encryption", "Quality"])
+
+	prev_mac = ''
+	wap_list.each do |w|
+		next if w.mac == prev_mac
+        tbl << [w.mac, w.ssid, w.power, w.channel, w.encryption, w.quality]
+		prev_mac = w.mac
     end
     
     puts tbl.to_s
-else
-    waps.each do |k, v|
-        tbl = RexTable::Table.new('Header' => k, 'Columns' => ["SSID", "RSSI", "Channel", "Encryption", "Quality"])
-        v.each do |w|
-            tbl << [w.ssid, w.power, w.channel, w.encryption, w.quality]
+end
+
+if options[:format].downcase == 'long'
+	waps = split_wap_list(wap_list, options[:sort])
+	
+	waps.keys.sort.each do |k|
+		
+        tbl = RexTable::Table.new(	'Header' => k, 
+									'Columns' => ["MAC", "SSID", "Power", "Channel", "Encryption", "Quality"])
+
+		prev_mac = ''
+		waps[k].each do |w|
+			next if w.mac == prev_mac
+            tbl << [w.mac, w.ssid, w.power, w.channel, w.encryption, w.quality]
+			prev_mac = w.mac
         end
+		
         puts tbl.to_s
         puts
         puts
