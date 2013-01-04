292a4eed4065e40cc10e49f3eb2b30a37de841b6
Stephen Haywood <stephen@averagesecurityguy.info>
2013-01-03 22:33:57 -0500

    Added licens.

--- ssh_audit.py
+++ ssh_audit.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013, AverageSecurityGuy
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this 
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice, 
+#  this list of conditions and the following disclaimer in the documentation 
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of AverageSecurityGuy nor the names of its contributors may
+#  be used to endorse or promote products derived from this software without
+#  specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+# OF SUCH DAMAGE.
+
 import os
 import re
 
-"""
-This script is dedicated to Shaun Waterman and Tatu Ylonen and should only be 
-used for legal purposes not for wiping out "most of the data on the servers of 
-every company in the developed world." To understand the dedication and the 
-sarcasm througout the comments, you will need to read this article: 
-
-http://www.washingtontimes.com/news/2012/dec/25/glitch-imperils-swath-of-encrypted-records/?page=all
-"""
 
 re_login = re.compile(r'Please login as the user "(.*)" rather than')
 
@@ -75,7 +95,7 @@ def add_hosts():
 	'''
 	pass
 
-	
+
 def get_ssh_keys(resp):
 	'''
 	Parse the response and get a list of SSH keys in the .ssh directory.
