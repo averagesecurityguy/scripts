9ccf498725119f679a64fe5c63e53a41bcf27f6c
Stephen Haywood <stephen@averagesecurityguy.info>
2013-03-28 23:37:17 -0400

    Use Queue to hold credentials.

--- multi_ssh.py
+++ multi_ssh.py
@@ -1,40 +1,64 @@
 import multiprocessing
+import paramiko
 import sys
+import time
+import Queue
 
 
-def check_ssh(password):
-    pass
+def worker(ip, cred_queue):
+    while True:
+        try:
+            creds = cred_queue.get(timeout=10)
+        except Queue.Empty:
+            return 
 
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(ip, username=creds[0], password=creds[1])
+            print 'Success: {0} {1} {2}'.format(ip, creds[0], creds[1])
+        except paramiko.AuthenticationException:
+            print 'Fail: {0} {1} {2}'.format(ip, creds[0], creds[1])
+        except Exception, e:
+            print 'Fail: {0} {1}'.format(ip, str(e))
+            cred_queue.put(creds)
+            return
 
-def worker(ip, password):
-    """
-    The worker function, invoked in a process. 'nums' is a
-    list of numbers to factor. The results are placed in
-    a dictionary that's pushed to a queue.
-    """
-    if check_ssh(ip, password) is True:
-        print 'Success: {0} - {1}'.format(ip, password)
+        time.sleep(.5)
 
+def file_to_list(filename):
+    data = []
+    for line in open(filename, 'r'):
+        line = line.strip()
+        if line == '':
+            continue
+        data.append(line)
+    return data
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print 'Usage: multi_ssh.py server_file password_file'
+    if len(sys.argv) != 4:
+        print 'Usage: multi_ssh.py server_file username_file password_file'
         sys.exit()
 
-    servers = [line.strip() for line in open(sys.argv[1], 'r')]
-    passwords = [line.strip() for line in open(sys.argv[2], 'r')]
+    servers = file_to_list(sys.argv[1])
+    usernames = file_to_list(sys.argv[2])
+    passwords = file_to_list(sys.argv[3])
 
+    cred_queue = multiprocessing.Queue()
     threads = len(servers)
-    chunksize = int(len(passwords)/float(threads))
     procs = []
 
     for i in range(threads):
-        p = multiprocessing.Process(
-            target=worker,
-            args=(passwords[chunksize * i:chunksize * (i + 1)]))
+        p = multiprocessing.Process(target=worker,
+                                    args=(servers[i], cred_queue))
         procs.append(p)
         p.start()
+
+    for user in usernames:
+        for pwd in passwords:
+            cred_queue.put((user, pwd))
 
     # Wait for all worker processes to finish
     for p in procs:
         p.join()
+
