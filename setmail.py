c64f2b34ee66b2cf072638c9750321c4b93165ae
Stephen Haywood <stephen@averagesecurityguy.info>
2014-06-19 19:53:28 -0400

    Catch smtplib exception and move to the next recipient.

--- setmail.py
+++ setmail.py
@@ -39,22 +39,32 @@ def get_mx_record(domain):
     records = dns.resolver.query(domain, 'MX')
     return str(records[0].exchange)
 
+
 class CustomSMTPServer(smtpd.SMTPServer):
     
     def process_message(self, peer, mailfrom, rcpttos, data):
         for rcptto in rcpttos:
+            print '[*] Sending message to {0}.'.format(rcptto)
+
             domain = rcptto.split('@')[1]
-            print domain
             mx = get_mx_record(domain)
-            print mx
-            server = smtplib.SMTP(mx, 25)
-            if debug:
-                server.set_debuglevel(True)
+            
             try:
+                server = smtplib.SMTP(mx, 25)
+                if debug:
+                    server.set_debuglevel(True)
                 server.sendmail(mailfrom, rcptto, data)
-            finally:
-                server.quit()
+
+            except smtplib.SMTPDataError as e:
+                print '[-] {0}'.format(str(e[1]))
+
+            except smtplib.SMTPServerDisconnected as e:
+                print '[-] {0}'.format(str(e))
+
+            except smtplib.SMTPConnectError as e:
+                print '[-] {0}'.format(str(e[1]))
+
 
 server = CustomSMTPServer(('127.0.0.1', port), None)
-print 'Server listening on port {0}'.format(port)
+print '[+] Server listening on port {0}'.format(port)
 asyncore.loop()
