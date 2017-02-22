3e07a8657912e031b1f2617b3ab8975d6e673415
averagesecurityguy <stephen@averagesecurityguy.info>
2017-02-21 20:02:54 -0500

    Find world writeable directories in boot scripts.

---
+++ bad_service.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+# Copyright 2017 AverageSecurityGuy
+#
+# Simple script to find world writeable directories that are also present
+# in bootup scripts. Executables in these directories may provide a way to
+# escalate privileges.
+#
+
+search ()
+{
+    ires=$(grep $1 /etc/init.d/*)
+    rres=$(grep -R $1 /etc/rc*)
+    cres=$(grep -R $1 /etc/cron*)
+    sres=$(grep -R $1 /etc/systemd/*)
+
+    if [ -n "$ires" ] || [ -n "$rres" ] || [ -n "$cres" ] || [ -n "$sres" ]
+    then
+        echo "\n$1"
+        echo "----"
+
+        if [ -n "$ires" ]; then echo $ires | tr ' ' "\n"; fi
+        if [ -n "$rres" ]; then echo $rres | tr ' ' "\n"; fi
+        if [ -n "$cres" ]; then echo $cres | tr ' ' "\n"; fi
+        if [ -n "$sres" ]; then echo $sres | tr ' ' "\n"; fi
+        echo
+    fi
+}
+
+
+files=$(find / -perm -0002 -type d -print)
+
+for d in $files; do
+    case "$d" in 
+        */tmp*) continue ;;
+        */run*) continue ;;
+        */dev*) continue ;;
+        */proc*) continue ;;
+        *) search "$d" ;;
+    esac
+done;
